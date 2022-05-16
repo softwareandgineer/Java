669c5364130e0332c04f4d84a7ad1479d4b53964
softwareandgineer <reachouttoandylee@gmail.com>
2022-05-15 23:03:56 -0400

    loops

--- LearnJava/src/ControlStatements.java
+++ LearnJava/src/ControlStatements.java
@@ -1,5 +1,71 @@
 public class ControlStatements {
 
+    public static void testMultiplicationTable()
+    {
+        for(int i = 1; i  <= 9; i++)
+        {
+            for(int j = 1; j <= 99; j++)
+            {
+                if(i >= j) {
+                    int result = i * j;
+                    System.out.print("" + i + " * " + j + " = " + result + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void testDoWhile() throws java.io.IOException
+    {
+        char result = 'k';
+        char ch;
+        boolean isNewLine = false;
+        do {
+            if(!isNewLine)
+                System.out.print("Press a key to guess");
+            ch = (char)System.in.read();
+            if(ch == '\n') {
+                isNewLine = true;
+                continue;
+            }
+            else
+                isNewLine = false;
+            if(ch == result)
+            {
+                System.out.print("Yes, you got it");
+                break;
+            }
+            else
+            {
+                System.out.println("You got it wrong nigga");
+            }
+        } while(ch != 'x');
+    }
+    public static void testPowers()
+    {
+        // print powers of 2 from 0 to  9 - 0^2 1^2 ...
+        for(int i = 0; i < 10; i++)
+        {
+            int e = i;
+            int result = 1;
+            while(e > 0)
+            {
+                result *= 2;
+                e--;
+            }
+            System.out.println("i=" + i + ", power of 2 is " + result);
+        }
+    }
+    public static void testWhile()
+    {
+        //print char from 'a' to 'z' using while
+        char ch = 'a';
+        while(ch <= 'z')
+        {
+            System.out.println(ch);
+            ch++;
+        }
+    }
+
     public static void testSwitch() throws java.io.IOException
     {
         // Simplified nested if else
@@ -59,8 +125,12 @@ public class ControlStatements {
 
     public static void main(String args[]) throws java.io.IOException
     {
-        testIfs();
-        testFor();
-        testSwitch();
+        //testIfs();
+        //testFor();
+        //testSwitch();
+        //testWhile();
+        //testPowers();
+        //testDoWhile();
+        testMultiplicationTable();
     }
 }
